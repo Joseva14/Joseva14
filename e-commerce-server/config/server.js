4692d6ad8f3fb2c99ca6b9d09bc4a8aafefc8626
Sebastian <joseva537.sv@gmail.com>
2021-03-29 10:56:11 -0500

    Se agrega Proyecto

---
+++ e-commerce-server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fe5ea180850f94b95235d1cd3e151269'),
+    },
+  },
+});
---
+++ e-commerce-server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8c30dcd4-96d2-4405-9ddd-0e9c3b028a16'
+};
